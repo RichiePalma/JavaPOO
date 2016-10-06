6f69e6c494098da30bfb6acac48c39ac3cebf217
RichiePalma <palmamendoza.5@hotmail.com>
2016-10-06 10:20:46 -0500

    Add files via upload

--- Division.java
+++ Division.java
@@ -1,5 +1,8 @@
 import javax.swing.JOptionPane;
-
+/* Autor: Ricardo Daniel Palma Mendoza 
+ * Clase: Division
+ * Fecha 05/10/2016
+ */
 public class Division {
 	
 	public static int divisionEntera(int numerador, int denominador){
@@ -11,23 +14,24 @@ public class Division {
 			int num=Integer.parseInt(JOptionPane.showInputDialog("Escribe el numerador"));
 			int den=Integer.parseInt(JOptionPane.showInputDialog("Escribe el denominador"));
 			if(den == 0){
-				throw new IllegalArgumentException("No se puede dividir entre 0"); 
+				throw new ArithmeticException("No se puede dividir entre 0"); 
 			}
 			else if(den > 0 || den < 0){
 				JOptionPane.showMessageDialog(null, "El resultado de dividir "+num+"/"+den+" es "+divisionEntera(num,den));	
 			}
 			else{
-				throw new NumberFormatException("Favor de insertar un numero y no una letra");
+				throw new NumberFormatException("Favor de insertar un numero y no una letra"); //El unico fallo es que esto no lo arroja
 			}
 		}
-		catch(IllegalArgumentException e){ // java.lang.IllegalArgumentException: por dividir entre 0 
+		catch(Exception e){ // Arithmetic Exception para las divisiones entre 0 aun que exception es más general
 			System.out.println("Ocurrio un error "+e);
 		} 
-		catch(NumberFormatException e){
+		/*catch(NumberFormatException e){
 			System.out.println("Ocurrio un error en " +e);
-		}
+		}*/
 	}
 	//Ctrl+A = Seleccionar todo Ctrl+I=Indent all
 	//Subir antes del jueves si el usuario pone algun dato invalido que mande una excepción (Tal como div 0 o String)
 	// java.lang.NumberFormatException: error por un String
+	//IllegalArgumentException salía antes, despues comenzo a salir Arithmetic Exception
 }
--- EjemploLector.java
+++ EjemploLector.java
@@ -1,3 +1,7 @@
+/* Autor: Ricardo Daniel Palma Mendoza 
+ * Clase: EjemploLector
+ * Fecha 05/10/2016
+ */
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,6 +16,7 @@ public class EjemploLector {
 			String ruta=JOptionPane.showInputDialog("Dame la ruta del archivo a leer");
 			BufferedReader br=new BufferedReader(new FileReader(ruta)); //No tienes unidad D
 			//C:\\Users\\Ricardo Palma\\Documents\\2ndo Semestre\\entrada2.txt
+			// se pone doble linea "\\" porque si solo pones "\" se confunde con un escape, por ejemplo "\n" es salto de linea
 			String linea; //Declaras existencia de linea y despues indicas que linea tiene el valor de string de cada linea del documento
 			while((linea = br.readLine()) != null){ //Mientras haya algo que leer en una linea, lee e imprime tal linea
 				System.out.println(linea);
